271cab04b3b3b805e7a28ec8d6544cdd09ea7b97
Andrey Ivanov <ivanov.as@phystech.edu>
2020-01-22 12:12:48 +0300

    Everything but collision probability

---
+++ app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Lib
+import Plant
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+
+main :: IO ()
+main = playIO canvas white 4 initPlant (return . renderPlant) updatePlantEvent updatePlantTime
+--display canvas white (renderPlant initPlant)
