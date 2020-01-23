f6f4b427a14e231132837f814297062a36bf8f51
Andrey Ivanov <ivanov.as@phystech.edu>
2020-01-23 17:15:46 +0300

    fixed shift, improved interacion

--- src/Plant.hs
+++ src/Plant.hs
@@ -17,6 +17,7 @@ data Plant = Plant { source   :: Point
                    , flag     :: Float
                    , lPressed :: Bool
                    , speed    :: Float
+                   , lCompton :: Float
                    } deriving (Show, Eq)
 
 --display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
@@ -33,7 +34,7 @@ punktline [(x1, y1), (x2, y2)] = pictures $ zipWith (\x y -> line [(x, y), (x+dx
 
 canvas = (InWindow "Compton's plant" size offset)
 
-initPlant = Plant (-128, 0) (8, 12) (120, 30) 640 0 False 1
+initPlant = Plant (-128, 0) (8, 12) (120, 30) 500 0 False 1 30
 
 renderPlant :: Plant -> Picture
 renderPlant plant = mappend txt $ scale 2.8 2.8 $ pictures [p1, p2, p3, p4, beam1, beam2, beam3]
@@ -41,7 +42,7 @@ renderPlant plant = mappend txt $ scale 2.8 2.8 $ pictures [p1, p2, p3, p4, beam
           (i, o) = target plant
           (r, t) = counter plant
           lbd = lambda plant
-          lc = if lPressed plant then 2.4e-3 else (lbd - 380) / 2
+          lc = if lPressed plant then 2.4e-3 else lCompton plant
           p0 = punktline [(-120, 0), (128, 0)]
           p1 = translate x y $ rectangleWire 16 16
           p2 = pictures [circleSolid i, circle o]
@@ -52,21 +53,17 @@ renderPlant plant = mappend txt $ scale 2.8 2.8 $ pictures [p1, p2, p3, p4, beam
           beam1 = color (colorWL lbd) $ translate (-64) 0 $ rectangleSolid 112 3
           beam2 = color (colorWL lbd') $ rotate (-t) $ translate 64 0 $ rectangleSolid 112 3
           beam3 = scale 1.0 3.0 $ color (colorWL lbd) $ punktline [(-8,0), (r,0)]
-          lbd' = lbd - lc*(1 - cos phi)
-          txt = pictures $! [ translate (-630) (-300) $ scale 0.2 0.2 $ Text $ "THETA = " ++ show (round t)
-                            , translate (-630) (-330) $ scale 0.2 0.2 $ Text $ "LAMBDA = " ++ show (round lbd)
-                            , translate (-630) (-360) $ scale 0.2 0.2 $ Text $ "LAMBDA_C = " ++ show (round lc)
+          lbd' = lbd + lc*(1 - cos phi)
+          txt = pictures $! [ translate (-630) (-270) $ scale 0.2 0.2 $ Text $ "THETA = " ++ show (round t)
+                            , translate (-630) (-300) $ scale 0.2 0.2 $ Text $ "LAMBDA = " ++ show (round lbd)
+                            , translate (-630) (-360) $ scale 0.2 0.2 $ Text $ "LAMBDA_C = " ++ show lc
+                            , translate (-630) (-330) $ scale 0.2 0.2 $ Text $ "LAMBDA' = " ++ show lbd'
                             ]
---translate 0 360 $ scale 0.2 0.2 $ Text $ "θ = " ++ show (round t) ++ "°\n λ = " ++ show lbd ++ " nm\n λc = " ++ show lc ++ " nm"
 
---lc = 132 --2.4e-3
-{-
- -rollDice :: IO Int
- rollDice = getStdRandom (randomR (1,6))
- - -}
+
 
 genAngle :: IO Int
-genAngle = getStdRandom (randomR (1, 359))
+genAngle = getStdRandom (randomR (0, 359))
 
 updatePlantTime :: Float -> Plant -> IO Plant
 updatePlantTime t plant = do
@@ -84,12 +81,23 @@ updatePlantEvent event plant =
         EventKey (Char l) Down _ _ -> do
             let lp = lPressed plant
             return plant {lPressed = not lp}
+        EventKey (SpecialKey KeyUp) Down (Modifiers _ Down _) _ -> do
+            let lc = lCompton plant
+                lbd = lambda plant
+            return $! plant {lCompton = min ((700 - lbd)/2) (lc + 5)}
+        EventKey (SpecialKey KeyDown) Down (Modifiers _ Down _) _ -> do
+            let lc = lCompton plant
+                lbd = lambda plant
+            return $! plant {lCompton = max 0 (lc - 5)}
         EventKey (SpecialKey KeyUp) Down _ _ -> do
             let lbd = lambda plant
-            return $! plant {lambda = min 750 (lbd + 10)}
+                lc  = lCompton plant
+                lp  = lPressed plant
+            return $! plant { lambda = if lp then 2.4e-3 else 
+                if lbd + max 10 (2*lc) <= 700 then lbd + 10 else lbd}
         EventKey (SpecialKey KeyDown) Down _ _ -> do
             let lbd = lambda plant
-            return $! plant {lambda = max 380 (lbd - 10)}
+            return $! plant {lambda = if (lPressed plant) then 2.4e-3 else max 380 (lbd - 10)}
         EventKey (SpecialKey KeyLeft) Down _ _ -> do
             let spd = speed plant
             return $! plant {speed = max 0.1 (spd-0.1)}
